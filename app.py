26d5610c84be12199c785a79758c76b947360afb
Gabriel <gabrielignaciosanchez99@gmail.com>
2022-06-26 13:07:50 -0300

    env

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from routes.contacts import contacts
 from flask_sqlalchemy import SQLAlchemy
-from config import DATABASE_CONNECTION_URI
+from config import DATABASE_CONNECTION_URI, SECRET_KEY
 
 app = Flask(__name__)
 
-app.secret_key = "secret key"
+app.secret_key = SECRET_KEY
 app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_CONNECTION_URI
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
--- config.py
+++ config.py
@@ -7,5 +7,7 @@ user = os.environ["MYSQL_USER"]
 password = os.environ["MYSQL_PASSWORD"]
 host = os.environ["MYSQL_HOST"]
 database = os.environ["MYSQL_DATABASE"]
+secret_key = os.environ["SECRET_KEY"]
 
 DATABASE_CONNECTION_URI = f"mysql://{user}:{password}@{host}/{database}"
+SECRET_KEY = f"{secret_key}"
